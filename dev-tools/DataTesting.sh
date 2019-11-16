585e68e5b9bc95588170c8e5d4e247c64220b582
spirillen <44526987+spirillen@users.noreply.github.com>
2019-11-16 01:10:27 +0100

    PyFunceble just don't understand multiline :unamused:

--- dev-tools/DataTesting.sh
+++ dev-tools/DataTesting.sh
@@ -40,10 +40,7 @@ RunFunceble () {
         rm "${pyfuncebleProductionConfigurationFileLocation}"
     fi
 
-    PyFunceble --travis -h -m -p 3 -db -ex --dns 127.0.0.1 \
-    --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" --plain \
-    --autosave-minutes 20 --commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}  [Auto Saved]" \
-    --commit-results-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [ci skip]" -f ${testfile}
+    PyFunceble --travis -h -m -p 3 -db -ex --dns 127.0.0.1 --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" --plain --autosave-minutes 20 --commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}  [Auto Saved]" --commit-results-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [ci skip]" -f ${testfile}
 
 }
 
