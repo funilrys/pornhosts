7c73f73e3c311b98c857633572a0769421c86dd5
Spirillen <pdns@protonmail.com>
2019-12-13 16:03:11 +0100

    Travis vs our script v.3 man vs the machine

--- dev-tools/DataTesting.sh
+++ dev-tools/DataTesting.sh
@@ -44,31 +44,20 @@ RunFunceble () {
 		--dns 127.0.0.1 --autosave-minutes 20 --share-logs\
 		--ci-branch pyfunceble-processing \
 		--ci-distribution-branch master \
-		--commit-autosave-message "${TAG}  [Auto Saved]" \
+		--commit-autosave-message "${TAG} [Auto Saved]" \
 		--commit-results-message "${TAG} [ci skip]" \
-		--cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" \
+		--cmd-before-end "mysqldump --user=pyfunceble --password=pyfunceble --opt pyfunceble > ${TRAVIS_BUILD_DIR}/db/pyfunceble.sql && bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" \
 		-f ${testfile}
 
 }
 
 RunFunceble
 
-UpLoadDb () {
-mkdir -p ${TRAVIS_BUILD_DIR}/db/
-mysqldump --user=pyfunceble --password=pyfunceble --opt pyfunceble > ${TRAVIS_BUILD_DIR}/db/pyfunceble.sql
-mysqldump --user=pyfunceble --password=pyfunceble --opt pyfunceble > ${TRAVIS_BUILD_DIR}/dev-tools/pyfunceble.sql
 
+#mkdir -p ${TRAVIS_BUILD_DIR}/db/
+#mysqldump --user=pyfunceble --password=pyfunceble --opt pyfunceble > ${TRAVIS_BUILD_DIR}/db/pyfunceble.sql
+#mysqldump --user=pyfunceble --password=pyfunceble --opt pyfunceble > ${TRAVIS_BUILD_DIR}/dev-tools/pyfunceble.sql
 
-git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-git config --global user.email "${GIT_EMAIL}"
-git config --global user.name "${GIT_NAME}"
-git config --global push.default simple
-git checkout "${GIT_BRANCH}"
-git add ${TRAVIS_BUILD_DIR}/dev-tools/pyfunceble.sql
-git commit -m "update sql"
-git push origin HEAD:${TRAVIS_BRANCH}
-}
-UpLoadDb
 
 ls -lh ${TRAVIS_BUILD_DIR}/db/
 
