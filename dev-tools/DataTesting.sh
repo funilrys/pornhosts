694257dba388356fab1a49f78ecbf66c6921ee3d
Spirillen <pdns@protonmail.com>
2019-12-14 12:17:13 +0100

    Machine vs man v.14

--- dev-tools/DataTesting.sh
+++ dev-tools/DataTesting.sh
@@ -40,10 +40,10 @@ RunFunceble () {
         rm "${pyfuncebleProductionConfigurationFileLocation}"
     fi
 
-    PyFunceble --ci -h -m -p 4 -db --database-type mariadb -ex --plain \
+    PyFunceble --travis -h -m -p 4 -db --database-type mariadb -ex --plain \
 		--dns 127.0.0.1 --autosave-minutes 20 \
-		--ci-branch pyfunceble-processing \
-		--ci-distribution-branch master \
+		--travis-branch pyfunceble-processing \
+		--travis-distribution-branch master \
 		--commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [Auto Saved]" \
 		--commit-results-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [ci skip]" \
 		--cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" \
