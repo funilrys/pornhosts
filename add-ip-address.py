c0883250a5720eaa39fa4d56086e23f9c4a19a48
spirillen <44526987+spirillen@users.noreply.github.com>
2020-01-01 23:17:05 +0100

    Delete add-ip-address.py

--- add-ip-address.py
+++
@@ -1,7 +0,0 @@
-# This script will add "0.0.0.0" at the beginning of each line
-
-import fileinput
-import sys
-
-for line in fileinput.input(['./new-list'], inplace=True):
-    sys.stdout.write('0.0.0.0 {l}'.format(l=line))
