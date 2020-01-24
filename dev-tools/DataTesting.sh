48aff9e05d14b9b61a89a628e1410bd904a4e733
spirillen <44526987+spirillen@users.noreply.github.com>
2020-01-24 19:44:42 +0100

    Update DataTesting.sh

--- dev-tools/DataTesting.sh
+++ dev-tools/DataTesting.sh
@@ -43,7 +43,7 @@ RunFunceble () {
     fi
 
         PyFunceble --ci -h -m -p $(nproc --ignore=1) -db --database-type mariadb -ex --plain --dns 127.0.0.1 \
-            --autosave-minutes 40 --share-logs --http --idna --ci-branch master \
+            --autosave-minutes 40 --share-logs --http --idna --ci-branch 'pyfunceble-processing' \
             --ci-distribution-branch master --hierarchical \
             --cmd-before-end "echo 'Final Commit' && bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" \
             --commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [Auto Saved]" \
