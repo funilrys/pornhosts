391dd37c1e9daf2901dc48db683f29a421e49d2f
The Unknown <5105020+AnonymousPoster@users.noreply.github.com>
2020-06-09 23:41:14 +0200

    Update setup_recursor.sh

--- dev-tools/setup_recursor.sh
+++ dev-tools/setup_recursor.sh
@@ -54,10 +54,30 @@ fi
 
 if drill 21x.org @127.0.0.1 -p 5300 | grep -qi "NXDOMAIN"
 then
-	printf "\n\tPirated domains Response policy zone My Privacy DNS is loaded... :smiley:\n\n"
+	printf "\n\tPirated domains Response policy zone My Privacy DNS is loaded... Good\n\n"
 else
 	printf "\t\nResponse policy zone not loaded, we are done for this time\n\n"
 	exit 1
 fi
 
 exit ${?}
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the modified GNU Affero General Public License as
+# published by the My Privacy DNS, either version 3 of the
+# License, or any later version released at
+# https://www.mypdns.org/w/License.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# modified GNU Affero General Public License for more details.
+
+# You should have received a copy of the modified GNU Affero General Public License
+# along with this program. If not, see https://www.mypdns.org/w/License.
+
+# The modification: The standard AGPLv3 have been altered to NOT allow
+# any to generate profit from our work. You are however free to use it to any
+# NON PROFIT purpose. If you would like to use any of our code for profiteering
+# YOU are obliged to contact https://www.mypdns.org/ for profit sharing.
