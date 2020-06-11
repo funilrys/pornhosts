976afb157a023a8b8e6f3e92e9502cfea212d01a
spirillen <44526987+spirillen@users.noreply.github.com>
2020-06-11 17:41:24 +0200

    Update PrepareData.sh

--- dev-tools/PrepareData.sh
+++ dev-tools/PrepareData.sh
@@ -18,11 +18,23 @@
 # **********************************
 
 # Type the url of source here
-#SOURCE=""
-hostsFile="${TRAVIS_BUILD_DIR}/submit_here/hosts.txt"
-snuff="${TRAVIS_BUILD_DIR}/submit_here/snuff.txt"
 testFile="${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt"
 
+# **************************************************************************
+# Sort lists alphabetically and remove duplicates before cleaning Dead Hosts
+# **************************************************************************
+getNewList () {
+	dig +noidnout axfr @35.156.219.71 -p 53 porn.host.srv \
+	  | grep -vE "^(;|$|\*)" | sed -e 's/porn\.host\.srv\.//g' \
+	  | awk '{ printf ("%s\n",tolower($1))}' \
+	  | sed -e 's/\.$//g' > "${testFile}"
+}
+
+head "${testFile}"
+
+# ***********************************
+# Deletion of all whitelisted domains
+# ***********************************
 # This following should be replaced by a local whitelist
 
 WhiteList="${TRAVIS_BUILD_DIR}/whitelist"
@@ -34,63 +46,18 @@ getWhiteList () {
     | awk '{ printf("ALL %s\n",tolower($1)) }' >> "${WhiteList}"
     sort -u -f "${WhiteList}" -o "${WhiteList}"
 }
-getWhiteList
-
-# *********************************************
-# Get Travis CI Prepared for Committing to Repo
-# with the new --travis-commmit &
-#
-# *********************************************
-
-
-#PrepareTravis () {
-    # NOTE: Commented out because PyFunceble already handle that :-)
-    #git remote rm origin
-    #git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-    #git config --global user.email "${GIT_EMAIL}"
-    #git config --global user.name "${GIT_NAME}"
-    #git config --global push.default simple
-    #git checkout "${GIT_BRANCH}"
-    #mysql --user=pyfunceble --password=pyfunceble pyfunceble < ${HOME}/db/pyfunceble.sql
-    #git checkout -B pyfunceble-processing
-    #ulimit -u
-#}
-#PrepareTravis
-
-# **************************************************************************
-# Sort lists alphabetically and remove duplicates before cleaning Dead Hosts
-# **************************************************************************
-
-PrepareLists () {
-
-    mkdir -p "${TRAVIS_BUILD_DIR}/PULL_REQUESTS/"
-
-    cat "${snuff}" >> "${testFile}"
-    cat "${hostsFile}" >> "${testFile}"
-
-    sort -u -f "${hostsFile}" -o "${hostsFile}"
-    sort -u -f "${snuff}" -o "${snuff}"
-    sort -u -f "${testFile}" -o "${testFile}"
-
-    dos2unix "${testFile}"
-
- }
-PrepareLists
-
-head "${testFile}"
-
-# ***********************************
-# Deletion of all whitelisted domains
-# ***********************************
 
 WhiteListing () {
+	hash uhb_whitelist
+	mv "${testFile}" "${testFile}.tmp.txt"
+	uhb_whitelist -wc -m -p $(nproc --ignore=1) -w "${WhiteList}" -f "${testfile}.tmp.txt" -o "${testFile}"
+}
+
 if [[ "$(git log -1 | tail -1 | xargs)" =~ "ci skip" ]]
 	then
-		hash uhb_whitelist
-		mv "${testFile}" "${testFile}.tmp.txt"
-		uhb_whitelist -wc -m -p $(nproc --ignore=1) -w "${WhiteList}" -f "${testfile}.tmp.txt" -o "${testFile}"
+	getNewList && \
+	  getWhiteList && \
+	  WhiteListing
 fi
-}
-WhiteListing
 
 exit ${?}
